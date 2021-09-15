b12cf353282d11e44816668ae2c24bed3b550132
Petr Fedorov <petr.fedorov@phystech.edu>
2021-09-15 16:41:20 +0300

    Specification of Transmute::createReconstructor() operation has been created

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -i -t --rm -v /tmp/.X11-unix:/tmp/.X11-unix -v ${HOME}/repos/oberon:/oberon -v /tmp/oberon:/tmp/oberon -v /tmp/sm:/tmp/sm -v bouml:/root -e DISPLAY=${DISPLAY} -e QT_X11_NO_MITSHM=1 bouml bouml
+docker run  -d --rm -v /tmp/.X11-unix:/tmp/.X11-unix -v ${HOME}/repos/oberon:/oberon -v /tmp/oberon:/tmp/oberon -v /tmp/sm:/tmp/sm -v bouml:/root -e DISPLAY=${DISPLAY} -e QT_X11_NO_MITSHM=1 bouml bouml
